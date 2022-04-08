9ecba209618368c5e6e6a4a22ebf0e39b9d9be10
Vram-AH <vram3012@gmail.com>
2022-04-08 15:18:08 +0700

    Sekuensial dan Perulangan

---
+++ Dasar_sintaksis_joke_pemograman.py
@@ -0,0 +1,24 @@
+"""
+Semua sintaksis dasar bahasa pemograman terdiri dari:
+1. Sekuensial: langkah berurutan
+2. Percabangan: langkah lompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang sama berkali - kali selama/sampai kondisi terpenuhi
+"""
+
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu berkata, "Beli satu botol susu, dan jika ada telor beli 6 telor"')
+print('Maka budi berangkat ke toko"')
+print('Dan mulai berbelanja')
+
+#Percabangan
+jumlah_botol_susu = 173
+jumlah_telur = 1587
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+
+else:
+    print("Budi membeli 1 botol susu")
