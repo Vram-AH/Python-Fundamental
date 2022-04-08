35ac6022118f3d3e37f99e8e520423f6ea8e45c9
Vram-AH <vram3012@gmail.com>
2022-04-08 15:24:14 +0700

    Sekuensial dan Perulangan Sequential & Branching

--- Dasar_sintaksis_joke_pemograman.py
+++ Dasar_sintaksis_joke_pemograman.py
@@ -15,10 +15,10 @@ print('Dan mulai berbelanja')
 #Percabangan
 jumlah_botol_susu = 173
 jumlah_telur = 1587
+print(f"Jumlah botol susu {jumlah_botol_susu} botol")
+print(f"Jumlah telur {jumlah_telur} butir")
 
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
     if jumlah_telur == 0:
 
-else:
-    print("Budi membeli 1 botol susu")
