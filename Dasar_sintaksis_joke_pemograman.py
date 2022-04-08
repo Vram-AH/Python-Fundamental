51b8ca601a32d59956ad7824096b4faffb052946
Vram-AH <vram3012@gmail.com>
2022-04-08 15:34:28 +0700

    Sekuensial dan Perulangan Sequential & Branching

--- Dasar_sintaksis_joke_pemograman.py
+++ Dasar_sintaksis_joke_pemograman.py
@@ -10,11 +10,12 @@ print('Ibu berkata, "Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu berkata, "Beli satu botol susu, dan jika ada telor beli 6 telor"')
 print('Maka budi berangkat ke toko"')
-print('Dan mulai berbelanja')
+print('Dan mulai untuk berbelanja')
 
 #Percabangan
 jumlah_botol_susu = 173
 jumlah_telur = 1587
+print("saat di toko budi melihat ke rak dan melihat")
 print(f"Jumlah botol susu {jumlah_botol_susu} botol")
 print(f"Jumlah telur {jumlah_telur} butir")
 
